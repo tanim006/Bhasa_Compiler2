7b0434e3ad2d4c0d20f52b0646ce792e83334caa
Tanim <tanimahamed680@gmail.com>
2026-08-14 21:13:03 +0600

    Fix parser and add parser examples

--- src/main/java/com/bhasha/Main.java
+++ src/main/java/com/bhasha/Main.java
@@ -2,6 +2,8 @@ package com.bhasha;
 
 import com.bhasha.lexer.Lexer;
 import com.bhasha.lexer.Token;
+import com.bhasha.parser.Parser;
+import com.bhasha.ast.Stmt;
 
 import java.util.List;
 
@@ -10,17 +12,34 @@ public class Main {
     public static void main(String[] args) {
 
         String source = """
-                declare integer x;
+                declare integer x = 10;
                 x = 10 + 20;
                 print(x);
                 """;
 
+        // 1. Lexical analysis
         Lexer lexer = new Lexer(source);
-
         List<Token> tokens = lexer.tokenize();
+
+        System.out.println("=== TOKENS ===");
 
         for (Token token : tokens) {
             System.out.println(token);
+        }
+
+        // 2. Parsing
+        Parser parser = new Parser(tokens);
+        List<Stmt> statements = parser.parse();
+
+        System.out.println("\n=== PARSER ===");
+
+        if (parser.errors().isEmpty()) {
+            System.out.println("Parsing successful!");
+            System.out.println("Statements parsed: " + statements.size());
+        } else {
+            for (String error : parser.errors()) {
+                System.out.println(error);
+            }
         }
     }
 }
